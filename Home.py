9d236a268f4e6371241f872b3da275f69179a108
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-05-21 05:56:25 -0300

    refactor: :art: Added a short description about the project.

--- Home.py
+++ Home.py
@@ -7,12 +7,24 @@ st.set_page_config(
 
 st.title("Gender Classification Model")
 
+st.header("O que é esse projeto?")
+
+st.markdown("""
+Esse projeto é apenas um estudo sobre Machine Learning junto ao desenvolvimento de um web app com o Streamlit. 
+            """)
+
 st.header("Objetivo")
 
-st.markdown(
-    """
-Esse webapp foi feito por mim (Deivid Souza Santana) com o intuito de prática um pouco sobre Machine Learning, uma área que tenho grande curiosidade.
+st.markdown("""
+O objetivo desse projeto foi colocar em prática o que eu conhecia sobre Machine Learning e criar uma aplicação que tornasse esse modelo útil.
 
-O modelo a segui foi feito com base em um Dataset que fornece o nome de uma pessoa e o sexo associado (por exemplo: Deivid, Masculino). Dessa maneira, minha ideia foi criar um modelo que aprendesse a fazer essa classificação por conta própria.
-"""
-)
+O modelo consiste no algoritmo K-Nearest Neighbor aplicado a um conjunto de dados onde o nome e o sexo de uma pessoa são as principais colunas. A ideia é conseguir classificar o sexo a partir do nome da pessoa.
+        """)
+
+st.header("Como usar?")
+
+st.markdown("""
+A aplicação possui duas páginas principais (sem contar essa, a Home). A primeira chamada 'Train and Test' onde o usuário insere um conjunto de dados (qualquer um, porém tem um no github pronto) com o nome e sexo da pessoa e configura (de maneira limitada) o algoritmo para gerar seu próprio modelo (é completamente normal se o treinamento demorar). Nessa mesma página ainda é possível baixar o modelo após ele ser treinado.
+
+A outra página é o 'Use Model' onde o modelo baixado pelo usuário poderá ser utilizado para fazer previsões reais. A depender da acurácio do modelo gerado as previsões podem ser boas ou não.
+        """)
